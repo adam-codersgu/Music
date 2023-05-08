fbb844a5eaf04cbfcf95ecd7f70596cabf2c8fb4
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-05-08 14:19:28 +0100

    Initial commit

---
+++ app/src/main/java/com/codersguidebook/music/MusicDao.kt
@@ -0,0 +1,26 @@
+package com.codersguidebook.music
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface MusicDao {
+
+    @Delete
+    suspend fun delete(song: Song)
+
+    @Query("SELECT * from music_table ORDER BY song_title")
+    fun getSongsOrderByTitle(): LiveData<List<Song>>
+
+    @Query("SELECT * from music_library WHERE song_album_id = :albumId ORDER BY song_track")
+    suspend fun getSongsByAlbumIdOrderByTrack(albumId: String): List<Song>
+
+    @Query("SELECT * FROM music_table WHERE song_title LIKE :search OR song_artist LIKE :search OR song_album LIKE :search")
+    suspend fun getSongsLikeSearch(search: String): List<Song>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(song: Song)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateMusicInfo(song: Song)
+}
